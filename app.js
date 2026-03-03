9b360c2e553d1bff00597eff2ab543e39e24a83c
luisota0705 <luis.otavioinacio.pereira@escola.pr.gov.br>
2026-03-03 09:07:09 -0300

    ok

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { supabase } from './supabaseClient.js'
 
-// Recupera o usuÃ¡rio logado. Redireciona para login se nÃ£o estiver autenticado.
+// Recupera o usuÃ¡rio logado. Redireciona para login se nÃ£o
+//  estiver autenticado.
 async function getUser() {
   const { data: { user } } = await supabase.auth.getUser()
   if (!user) window.location.href = 'login.html'

84d428f6eba7d182828149b3105d462a5a88b1b9
luisota0705 <luis.otavioinacio.pereira@escola.pr.gov.br>
2026-03-03 09:13:22 -0300

    ff

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,6 +4,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-    'https://ylxqdwszxcvqsdklwmzg.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlseHFkd3N6eGN2cXNka2x3bXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE1MjEsImV4cCI6MjA4NzY4NzUyMX0.w75pSkWEk4F_43IQK5Y4DY53OqrO-8oIQGFj60uCvf4'
+  'https://ylxqdwszxcvqsdklwmzg.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlseHFkd3N6eGN2cXNka2x3bXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE1MjEsImV4cCI6MjA4NzY4NzUyMX0.w75pSkWEk4F_43IQK5Y4DY53OqrO-8oIQGFj60uCvf4'
 )
+    
